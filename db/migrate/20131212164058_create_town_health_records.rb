df1c439af0f686b4f30d7778c59f185267d62452
Helen Hood <helen.c.hood@gmail.com>
2013-12-12 11:50:29 -0500

    Created TownHealthRecords table, with not-null constraint on geography.

--- db/migrate/20131212164058_create_town_health_records.rb
+++ db/migrate/20131212164058_create_town_health_records.rb
@@ -1,6 +1,21 @@
 class CreateTownHealthRecords < ActiveRecord::Migration
   def change
     create_table :town_health_records do |t|
+      t.string :geography, null: false
+      t.integer :pop_total_2005
+      t.integer :pop_0_thru_19_2005
+      t.integer :pop_over65_2005
+      t.integer :per_cap_income_2000
+      t.integer :num_below_200fpl_2000
+      t.decimal :perc_below_200fpl_2000
+      t.decimal :perc_adeq_prenatal_care
+      t.decimal :perc_c_sections_2005_08
+      t.integer :num_infant_deaths_2005_08
+      t.decimal :infant_mortality_rate_per_1000_2005_08
+      t.decimal :perc_low_birthweight_2005_08
+      t.decimal :perc_multiple_births_2005_08
+      t.decimal :perc_pub_financed_prenatal_care_2005_08
+      t.decimal :perc_teen_births_2005_08
 
       t.timestamps
     end
